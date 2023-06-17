9098b8e78fdb6a44251615b1968fedbd8f1cbe1f
Tasya Dwi Askara <tasyadwiaskarasiahaan@gmail.com>
2023-06-17 08:11:55 +0700

    adding comment, sosmed route and its authorization

--- middlewares/authorization.go
+++ middlewares/authorization.go
@@ -46,3 +46,77 @@ func PhotoAuthorization() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CommentAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := database.GetDB()
+		commentId, err := strconv.Atoi(c.Param("comment_id"))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": "invalid parameter",
+			})
+			return
+		}
+		userData := c.MustGet("userData").(jwt.MapClaims)
+		userID := uint(userData["id"].(float64))
+		Comment := models.Comment{}
+
+		err = db.Select("user_id").First(&Comment, uint(commentId)).Error
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   "Data Not Found",
+				"message": "data doesn't exist",
+			})
+			return
+		}
+
+		if Comment.UserID != userID {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": "you are not allowed to access this data",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func SosmedAuthorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		db := database.GetDB()
+		sosmedId, err := strconv.Atoi(c.Param("socmed_id"))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": "invalid parameter",
+			})
+			return
+		}
+		userData := c.MustGet("userData").(jwt.MapClaims)
+		userID := uint(userData["id"].(float64))
+		Sosmed := models.Sosmed{}
+
+		err = db.Select("user_id").First(&Sosmed, uint(sosmedId)).Error
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   "Data Not Found",
+				"message": "data doesn't exist",
+			})
+			return
+		}
+
+		if Sosmed.UserID != userID {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": "you are not allowed to access this data",
+			})
+			return
+		}
+
+		c.Next()
+	}
+}
--- router/router.go
+++ router/router.go
@@ -21,9 +21,31 @@ func StartApp() *gin.Engine {
 	photoRouter := r.Group("/photos")
 	{
 		photoRouter.Use(middlewares.Authentication())
+		photoRouter.GET("/", controllers.GetAllPhoto)
 		photoRouter.POST("/", controllers.CreatePhoto)
 
 		photoRouter.PUT("/:photo_id", middlewares.PhotoAuthorization(), controllers.UpdatePhoto)
+		photoRouter.DELETE("/:photo_id", middlewares.PhotoAuthorization(), controllers.DeletePhoto)
+	}
+
+	commentRouter := r.Group("/comments")
+	{
+		commentRouter.Use(middlewares.Authentication())
+		commentRouter.GET("/", controllers.GetAllComment)
+		commentRouter.POST("/", controllers.CreateComment)
+
+		commentRouter.PUT("/:comment_id", middlewares.CommentAuthorization(), controllers.UpdateComment)
+		commentRouter.DELETE("/:comment_id", middlewares.CommentAuthorization(), controllers.DeleteComment)
+	}
+
+	sosmedRouter := r.Group("/socmeds")
+	{
+		sosmedRouter.Use(middlewares.Authentication())
+		sosmedRouter.GET("/", controllers.GetAllSosmed)
+		sosmedRouter.POST("/", controllers.CreateSocialMedia)
+
+		sosmedRouter.PUT("/:socmed_id", middlewares.SosmedAuthorization(), controllers.UpdateSocialMedia)
+		sosmedRouter.DELETE("/:socmed_id", middlewares.SosmedAuthorization(), controllers.DeleteSocialMedia)
 	}
 
 	return r
